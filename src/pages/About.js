0696394c5d37aa38121d90aafcf73394cf48dbe5
Faiq Shariff <faiq@srptechs.com>
2024-10-24 18:12:53 +0500

    Basic Structure of react initialized

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        This is a more complex React setup including routing, context API, and
+        API calls.
+      </p>
+    </div>
+  );
+};
+
+export default About;
